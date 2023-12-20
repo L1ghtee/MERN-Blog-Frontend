96be31f69cc0b09d874674b5547533af3a5b3dae
L1ghtee <nikorak20@gmail.com>
2023-12-20 15:26:13 +0200

    3:53

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-import Button from '@mui/material/Button';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
-import styles from './Header.module.scss';
-import Container from '@mui/material/Container';
+import styles from "./Header.module.scss";
+import Container from "@mui/material/Container";
+import { useDispatch, useSelector } from "react-redux";
+import { logout,selectIsAuth } from "../../redux/slices/auth";
 
 export const Header = () => {
-  const isAuth = false;
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if (window.confirm("Are you sure want to logout")) {
+      dispatch(logout());
+      window.localStorage.removeItem('token')
+    }
+  };
 
   return (
     <div className={styles.root}>
@@ -20,11 +28,15 @@ export const Header = () => {
           <div className={styles.buttons}>
             {isAuth ? (
               <>
-                <Link to="/posts/create">
+                <Link to="/add-post">
                   <Button variant="contained">Create Post</Button>
                 </Link>
-                <Button onClick={onClickLogout} variant="contained" color="error">
-                 Log out
+                <Button
+                  onClick={onClickLogout}
+                  variant="contained"
+                  color="error"
+                >
+                  Log out
                 </Button>
               </>
             ) : (
--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -27,30 +27,30 @@ const postSlice = createSlice({
   reducers: {},
   extraReducers: {
     [fetchPosts.pending]: (state) => {
-        state.posts.items = [];
-        state.posts.status = 'loading';
-      },
-      [fetchPosts.fulfilled]: (state, action) => {
-        state.posts.items = action.payload;
-        state.posts.status = 'loaded';
-      },
-      [fetchPosts.rejected]: (state) => {
-        state.posts.items = [];
-        state.posts.status = 'error';
-      },
+      state.posts.items = [];
+      state.posts.status = "loading";
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.posts.items = action.payload;
+      state.posts.status = "loaded";
+    },
+    [fetchPosts.rejected]: (state) => {
+      state.posts.items = [];
+      state.posts.status = "error";
+    },
 
-      [fetchTags.pending]: (state) => {
-        state.tags.items = [];
-        state.tags.status = 'loading';
-      },
-      [fetchTags.fulfilled]: (state, action) => {
-        state.tags.items = action.payload;
-        state.tags.status = 'loaded';
-      },
-      [fetchTags.rejected]: (state) => {
-        state.tags.items = [];
-        state.tags.status = 'error';
-      },
+    [fetchTags.pending]: (state) => {
+      state.tags.items = [];
+      state.tags.status = "loading";
+    },
+    [fetchTags.fulfilled]: (state, action) => {
+      state.tags.items = action.payload;
+      state.tags.status = "loaded";
+    },
+    [fetchTags.rejected]: (state) => {
+      state.tags.items = [];
+      state.tags.status = "error";
+    },
   },
 });
 
